c949aaef2116d00a424c0fb90c725366e8c71d94
Zaen Muttakin <zaenmuttakin@gmail.com>
2024-10-04 01:45:32 +0700

    change input bb

--- src/app/components/card/CardDetails.js
+++ src/app/components/card/CardDetails.js
@@ -13,20 +13,20 @@ export default function CardDetails() {
         <table className="text-left rtl:text-righ text-blue">
           <tbody>
             <tr className="">
-              <td className="pt-2">Tanggal</td>
-              <td className="pt-2 ps-4">27/09/2024</td>
+              <td className="pt-1.5">Tanggal</td>
+              <td className="pt-1.5 ps-4">27/09/2024</td>
             </tr>
             <tr className="">
-              <td className="pt-2">Pallet</td>
-              <td className="pt-2 ps-4">2</td>
+              <td className="pt-1.5">Pallet</td>
+              <td className="pt-1.5 ps-4">2</td>
             </tr>
             <tr className="">
-              <td className="pt-2 ">Checker</td>
-              <td className="pt-2 ps-4">Hasan</td>
+              <td className="pt-1.5 ">Checker</td>
+              <td className="pt-1.5 ps-4">Hasan</td>
             </tr>
             <tr className="">
-              <td className="pt-2 ">Mulai hitung</td>
-              <td className="pt-2 ps-4">12.30</td>
+              <td className="pt-1.5 ">Mulai hitung</td>
+              <td className="pt-1.5 ps-4">12.30</td>
             </tr>
           </tbody>
         </table>
--- src/app/pages/barang-bocor/input/bb-fg/page.js
+++ src/app/pages/barang-bocor/input/bb-fg/page.js
@@ -30,57 +30,59 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="main-container gap-4 bg-white rounded-b-3xl shadow-sm justify-between">
+    <div className="main-container gap-4 bg-white justify-between">
       <ModalConfirm
         color={confirmModalData.color}
         title={confirmModalData.title}
         yesLabel={confirmModalData.yesLabel}
         yesAction={confirmModalData.yesAction}
       />
-      <div className="pt-4 mb-3 gap-2 w-full flex flex-row items-center justify-between">
-        <h3 className=" font-bold flex-grow">Input Barang Bocor</h3>
-        <button
-          className="flex items-middle gap-2"
-          onClick={() => setDetails(!details)}
-        >
-          <p className="text-sm px-3 py-1 bg-blue rounded-xl text-white">
-            27/09/2024
-          </p>
-          <p className="text-sm px-3 py-1 mr-2 bg-blue rounded-xl text-white">
-            2
-          </p>
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            className={`duration-200 mx-auto ${details && "rotate-180"}`}
-          />
-        </button>
-      </div>
-      {details && <CardDetails />}
-      <div className="w-full  flex flex-col gap-4 justify-between">
-        <div className="flex flex-col gap-4">
-          <Alert text={"Diisi oleh checker finish good"} style={"mb-2"} />
+      <div className="w-full flex flex-col gap-4">
+        <div className="pt-4 mb-3 gap-2 w-full flex flex-row items-center justify-between">
+          <h3 className=" font-bold flex-grow">Input Barang Bocor</h3>
+          <button
+            className="flex items-middle gap-2"
+            onClick={() => setDetails(!details)}
+          >
+            <p className="text-sm px-3 py-1 bg-blue rounded-xl text-white">
+              27/09/2024
+            </p>
+            <p className="text-sm px-3 py-1 mr-2 bg-blue rounded-xl text-white">
+              2
+            </p>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`duration-200 mx-auto ${details && "rotate-180"}`}
+            />
+          </button>
+        </div>
+        {details && <CardDetails />}
+        <div className="w-full  flex flex-col gap-4 justify-between">
+          <div className="flex flex-col gap-4">
+            <Alert text={"Diisi oleh checker finish good"} style={"mb-2"} />
 
-          <CardInputCanvas plant="1015" />
-          <CardInputCanvas plant="1016" />
+            <CardInputCanvas plant="1015" />
+            <CardInputCanvas plant="1016" />
+          </div>
         </div>
-        <div className="flex flex-row gap-4 mt-2">
-          <BtnSubmit
-            title="Batal"
-            type="red-outline"
-            onClick={() => router.push("/pages/barang-bocor")}
-          />
-          <BtnSubmit
-            title="Simpan"
-            // onClick={() => {
-            //   setConfirmModalData({
-            //     color: "blue",
-            //     title: "Simpan data ini?",
-            //     yesLabel: "Simpan",
-            //   });
-            //   setOpenConfirmModal(true);
-            // }}
-          />
-        </div>
+      </div>
+      <div className="w-full flex flex-row gap-4 mt-2">
+        <BtnSubmit
+          title="Batal"
+          type="red-outline"
+          onClick={() => router.push("/pages/barang-bocor")}
+        />
+        <BtnSubmit
+          title="Simpan"
+          // onClick={() => {
+          //   setConfirmModalData({
+          //     color: "blue",
+          //     title: "Simpan data ini?",
+          //     yesLabel: "Simpan",
+          //   });
+          //   setOpenConfirmModal(true);
+          // }}
+        />
       </div>
     </div>
   );
